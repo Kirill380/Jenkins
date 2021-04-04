a3e219f5a3755c7c72671c47c475fddbd0dabb64
Kirill Liubun <kreol_123@ukr.net>
2021-04-04 17:04:36 +0300

    add integration with s3

--- src/main/java/io/kliubun/SimpleApplication.java
+++ src/main/java/io/kliubun/SimpleApplication.java
@@ -1,22 +1,47 @@
 package io.kliubun;
 
+import com.amazonaws.auth.InstanceProfileCredentialsProvider;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import org.apache.commons.io.Charsets;
+import org.apache.commons.io.IOUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 @RestController
 @SpringBootApplication
 public class SimpleApplication {
 
+    private AmazonS3 s3;
+
     public static void main(String[] args) {
         SpringApplication.run(SimpleApplication.class, args);
     }
 
-    @RequestMapping("/")
-    public String index() {
+    @PostConstruct
+    public void init() {
+       s3 = AmazonS3ClientBuilder.standard()
+                .withCredentials(InstanceProfileCredentialsProvider.getInstance())
+                .build();
 
-        return "Greetings from Spring Boot! 2113";
+    }
+
+    @RequestMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String index() throws IOException {
+        final S3Object simpleResponse = s3.getObject("igneuslynx", "simple.json");
+        final S3ObjectInputStream objectContent = simpleResponse.getObjectContent();
+        final String content = IOUtils.toString(objectContent, StandardCharsets.UTF_8);
+        return content;
     }
 
 }
