b423b025146bc8255f7dbc285df520434c408c0a
Kirill Liubun <kreol_123@ukr.net>
2021-04-07 19:26:52 +0300

    add extra key

--- src/main/java/io/kliubun/SimpleApplication.java
+++ src/main/java/io/kliubun/SimpleApplication.java
@@ -6,6 +6,8 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.io.Charsets;
 import org.apache.commons.io.IOUtils;
 import org.springframework.boot.SpringApplication;
@@ -18,12 +20,14 @@ import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.util.Map;
 
 @RestController
 @SpringBootApplication
 public class SimpleApplication {
 
     private AmazonS3 s3;
+    private ObjectMapper objectMapper = new ObjectMapper();
 
     public static void main(String[] args) {
         SpringApplication.run(SimpleApplication.class, args);
@@ -39,10 +43,14 @@ public class SimpleApplication {
     }
 
     @RequestMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
-    public String index() throws IOException {
+    public Object index() throws IOException {
         final S3Object simpleResponse = s3.getObject("igneuslynx", "simple.json");
         final S3ObjectInputStream objectContent = simpleResponse.getObjectContent();
-        final String content = IOUtils.toString(objectContent, StandardCharsets.UTF_8);
+        final Map<String, Object> content = objectMapper.readValue(objectContent,
+                new TypeReference<Map<String, Object>>() {
+                });
+
+        content.put("extraKey", "asasdasd");
         return content;
     }
 
